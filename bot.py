3ce82f040875cb5341794bce1866f8dd9edf6865
thefreshestprinceinbelair <160644104+thefreshestprinceinbelair@users.noreply.github.com>
2024-02-20 17:59:25 +0000

    e

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext.commands import Bot,Context
-from discord import Intents
+from discord import Intents, Message
 
-bot = Bot('>',intents=Intents(messages=True))
+bot = Bot('>',intents=Intents.all())
 
 
 # from here you can edit to make more commands
@@ -9,8 +9,19 @@ bot = Bot('>',intents=Intents(messages=True))
 
 @bot.command()
 async def ping(ctx:Context):
-    await ctx.send("pong")
+    await ctx.send("what the fuck why are you pinging me im tryna sleep")
 
+@bot.event
+async def on_message(msg:Message):
+    if msg.author.id != bot.user.id:
+        if msg.content.lower().startswith("im"):
+
+            await msg.channel.send(f"hi {msg.content[3:]}, i'm dad")
+        if msg.content.lower().startswith("i am"):
+
+            await msg.channel.send(f"hi {msg.content[5:]}, i'm dad")
+        if msg.content.lower().startswith("i'm"):
+            await msg.channel.send(f"hi {msg.content[4:]}, i'm dad")
 
 # do not touch after this
 try:
